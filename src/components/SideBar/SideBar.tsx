52801c11725f336d1bcf39884c18a217edc05c46
elmobbB <108806956+elmobbB@users.noreply.github.com>
2023-03-30 11:39:17 +0800

    feat: test

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -82,7 +82,7 @@ const SideBar = () => {
       <button onClick={showModalHandler}>
         <SideBarRow Icon={UserIcon} title="profile" />
       </button>
-      <SetIcon onClose={hideModalHandler} />
+      <SetIcon />
       {/* {showModal && <SetIcon onClose={hideModalHandler} />} */}
       <button
         onClick={signoutHandler}
